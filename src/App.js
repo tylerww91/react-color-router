40a334ed0f00582b0ee8b144ba53bd95b2769830
Tyler Watson <tylerww91@gmail.com>
2023-01-24 10:55:14 -0800

    builds routes for 3 color options in Header.js, builds switch within app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header/Header.js';
 
 function App() {
   return (
     <main className="App">
-      <Header />
+      <Switch>
+        <Route path="/RBG/:R/:G/:B" component={Header} />
+      </Switch>
     </main>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
 
 export default function Header() {
   return (
     <header className="header-nav">
-      <p>blue</p>
-      <p>red</p>
-      <p>green</p>
+      <p>
+        <NavLink to={`/RGB/92/109/112`}>Cadet</NavLink>
+      </p>
+      <p>
+        <NavLink to={`/RGB/163/119/116`}>Burnished Brown</NavLink>
+      </p>
+      <p>
+        <NavLink to={`/RGB/232/136/115`}>Middle Red</NavLink>
+      </p>
     </header>
   );
 }
